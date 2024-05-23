5260103c32a8ca0cffa67511c7a8e247e0c9917d
rajanshresth <shrestharajan726@gmail.com>
2024-05-23 22:46:09 +0545

    .auth redirect

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex items-center justify-center mt-8">
-      <SignIn />
+      <SignIn afterSignInUrl={"/course"} />
     </div>
   );
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex items-center justify-center mt-8">
-      <SignUp />
+      <SignUp afterSignUpUrl={"/course"} />
     </div>
   );
 }
