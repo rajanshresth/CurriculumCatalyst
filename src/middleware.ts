41eeb391d1fe88326350e8ea58fc7fcd92d220d9
rajanshresth <shrestharajan726@gmail.com>
2024-05-23 22:59:08 +0545

    middleware runtime=edge

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
+export const runtime = "edge";
 const isProtectedRoute = createRouteMatcher(["/course", "/admin"]);
 
 export default clerkMiddleware((auth, req) => {
